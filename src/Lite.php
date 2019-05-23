2a6366d211c36b167132f2325abad57ad6c1bc8e
JamesLiao <vivlonglz@gmail.com>
2019-05-23 16:29:30 +0800

    Update

--- src/Lite.php
+++ src/Lite.php
@@ -53,4 +53,56 @@ class Lite {
 		}
 		return -1;
 	}
+
+	/**
+	 * 推送接口
+	 */
+	public function push($device_id, $device_type, $content, $title, $extras) {
+		if(!empty($device_id)) {
+			$pusher = $this->client->push();
+			$cid = md5(implode(',', $extras)); //用于防止 api 调用端重试造成服务端的重复推送而定义的一个标识符。
+			//$pusher->setCid($cid);
+			$pusher->addRegistrationId(array($device_id));
+			$platform = array('all'); //推送平台设置
+			if(strpos('android', strtolower($device_type))) {
+				$platform = array('android');
+				$android_notification = array(
+					'title' => $title,
+					'category' => $cid,
+					'extras' => $extras
+				);
+				$pusher->androidNotification($content, $android_notification);
+			} else if(strpos('ios', strtolower($device_type))) {
+				$platform = array('ios');
+				$ios_notification = array(
+					'alert' => $content,
+					'badge' => '+1',
+					'thread-id' => $cid,
+					'extras' => $extras
+				);
+				$pusher->iosNotification($content, $ios_notification);
+			}
+			$pusher->message($title, [
+				'title' => $title,
+				'msg_content' => $content,
+				'content_type' => 'text',
+				'extras' => $extras
+			]);
+			$pusher->setPlatform($platform);
+			//推送参数
+			$options = array(
+				//'sendno' => md5(uniqid('', true)),
+				'time_to_live' => 0,
+			);
+			try {
+				$response = $pusher->options($options)->send();
+				return 1;
+			} catch (\JPush\Exceptions\APIConnectionException $e) {
+				\PhalApi\DI()->logger->log('JPush','APIConnectionException', array('error' => $e->__toString()));
+			} catch (\JPush\Exceptions\APIRequestException $e) {
+				\PhalApi\DI()->logger->log('JPush','APIRequestException', array('error' => $e->__toString()));
+			}
+		}
+		return -1;
+	}
 }
